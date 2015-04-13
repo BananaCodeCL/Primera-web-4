7eec7408f9d140fa8b026c29ad0d4e3aebd9220c
BananaCode <bananacodecl@gmail.com>
2015-04-12 21:47:46 -0300

    commit con php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>BananaCode - Ejemplo 4</title>
+	
+</head>
+<body>
+	<div class="cuerpo">
+		"Ahora trabajamos con PHP"
+		
+		<?php
+		
+		echo "Mensaje realizado con Php";
+		
+		
+		?>
+		
+		
+	</div>
+	<footer>Pie de Pagina</footer>
+</body>
+</html>
+
+
+
